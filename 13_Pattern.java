732f31bdb7d02012f6e332f3545679a0488b95b9
Ankit Kumar <krankit01012003@gmail.com>
2026-04-04 14:39:42 +0530

    I have added the resource of the DSA leacture and assigment

---
+++ 13_Pattern.java
@@ -0,0 +1,42 @@
+// https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/assignments/09-patterns.md
+
+class Pattern {
+    public static void main(String[] args) {
+        pattern4(4);
+    }
+
+    public static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n + 1 -row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
